dadc7189373ebcd7c6d514c3c43c1d0c83d4345c
Mert Ahmet Yiğit <mayigit@icloud.com>
2018-02-14 21:41:26 +0300

    Committing - Geliştirmeler Yapıldı.

--- Sanat Kitabı/DetayViewController.swift
+++ Sanat Kitabı/DetayViewController.swift
@@ -7,29 +7,126 @@
 //
 
 import UIKit
+import CoreData
 
-class DetayViewController: UIViewController {
+class DetayViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    var choosingArt = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if choosingArt != "" {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.choosingArt)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            
+            do {
+            
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject]{
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            
+                            let image = UIImage(data: imageData)
+                            self.imageView.image = image
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                
+                
+                
+            } catch {
+                print("error")
+            }
+            
+            
+        }
 
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecordnizer = UITapGestureRecognizer(target: self, action: #selector(DetayViewController.selectImage))
+        
+        imageView.addGestureRecognizer(gestureRecordnizer)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Kullanıcı Resmi Seçti.
+    @objc func selectImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //Kullanıcıdan Gelen Resmi ImageView'a Ekledik
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    */
+
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        newArt.setValue(nameText.text, forKey: "name")
+        newArt.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!) {
+            newArt.setValue(year, forKey: "year")
+        }
+        
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        newArt.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("successful")
+        } catch {
+            print("error")
+        }
+        
+        
+        
+    }
+    
+   
 
 }
--- Sanat Kitabı/ViewController.swift
+++ Sanat Kitabı/ViewController.swift
@@ -7,19 +7,123 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var nameArray = [String]()
+    var yearArray = [Int]()
+    var artistArray = [String]()
+    var imageArray = [UIImage]()
+    var secilenArt = ""
+    
+    
+    
+    
+    
+    func getInfo(){
+        
+        nameArray.removeAll(keepingCapacity: false)
+        yearArray.removeAll(keepingCapacity: false)
+        artistArray.removeAll(keepingCapacity: false)
+        imageArray.removeAll(keepingCapacity: false)
+        
+        
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    if let year = result.value(forKey: "year") as? Int {
+                        self.yearArray.append(year)
+                    }
+                    if let artist = result.value(forKey: "artist") as? String {
+                        self.artistArray.append(artist)
+                    }
+                    if let imageData = result.value(forKey: "image") as? Data {
+                        
+                        let image = UIImage(data: imageData)
+                            self.imageArray.append(image!)
+                         }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+            }
+            
+            
+            
+        } catch {
+            print("error")
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getInfo()
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        secilenArt = nameArray[indexPath.row]
+         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetayViewController
+            destinationVC.choosingArt = secilenArt
+        }
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        
+        secilenArt = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+        
+    }
+        
     }
 
 
-}
+
+
+    
+
 

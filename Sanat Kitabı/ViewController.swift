67685bba86d00359f0cbbcd0ddec2dbd3ee231bd
Mert Ahmet Yiğit <mayigit@icloud.com>
2018-02-14 19:07:56 +0300

    Initial Commit

---
+++ Sanat Kitabı/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sanat Kitabı
+//
+//  Created by Mert Ahmet Yiğit on 14.02.2018.
+//  Copyright © 2018 Mert Yiğit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
